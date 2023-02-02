1c7076d9ab5ec85ff07b5714daa17d1ce9391395
Krishan Pareek <krishanpareek809@gmail.com>
2023-02-03 01:25:29 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './pages/Homepage/Homepage';
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Homepage />
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/about-us" element={<AboutUs />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
